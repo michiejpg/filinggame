4b822e4727c69bbe3bf07be7cd4358a5005d153c
michiejpg <michiewinchester@gmail.com>
2025-11-18 11:00:02 -0800

    Change canvas size to 1920x1080

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ let fadingOut = false;
 let fadingIn = false;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1920, 1080);
   // Load high score from localStorage
   if (localStorage.getItem("highScore")) {
     highScore = int(localStorage.getItem("highScore"));
